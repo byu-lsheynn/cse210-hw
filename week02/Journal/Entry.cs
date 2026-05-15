445cf934d9db42ecfeb5a354bc857ae813aa072f
byu-ldsheynn <lvengco@byupathway.edu>
2026-05-15 20:15:43 +0800

    Add code on Entry class

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,8 +5,18 @@ public class Entry
     string _promptText;
     string _entryText;
 
+    public Entry()
+    {
+        PromptGenerator _prompt = new PromptGenerator();
+        _promptText = _prompt.GetRandomPrompt();
+    }
+
     public void Display()
     {
-        
+        Console.WriteLine(_promptText);
+        _entryText = Console.ReadLine();
+        _date = DateTime.Now.ToString();
+
+        //Console.WriteLine($"Saved on: {_date}");
     }
 }
