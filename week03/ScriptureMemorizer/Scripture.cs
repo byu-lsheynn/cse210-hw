f96088a75cabb874ea54895e33928987e3212c4c
byu-ldsheynn <lvengco@byupathway.edu>
2026-05-20 23:51:57 +0800

    Created classes, attended their attributes and constructors

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,10 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
+}
