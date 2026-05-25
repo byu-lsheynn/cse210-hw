90ab8f52d6249558fbb657fb3e764c958337cd17
byu-ldsheynn <lvengco@byupathway.edu>
2026-05-25 20:28:16 +0800

    Final program

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,10 +1,68 @@
+using System;
+
 public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
-    
+
     public Scripture(Reference reference, string text)
     {
-        
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitText = text.Split(' ');
+        foreach (string word in splitText)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        for (int i = 0; i < numberToHide && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+
+            visibleWords[index].Hide();
+
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText();
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
