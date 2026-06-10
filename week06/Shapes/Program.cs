300f198181bdbc833b054f209188897d751cfeca
byu-ldsheynn <lvengco@byupathway.edu>
2026-06-10 20:09:43 +0800

    W06 Learning Activity: Polymorphism completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Blue", 4);
+        Rectangle rectangle = new Rectangle("Red", 3, 2);
+        Circle circle = new Circle("Yellow", 2.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2} inches.\n");
+        }
     }
 }
